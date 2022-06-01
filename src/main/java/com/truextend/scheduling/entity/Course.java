da1a0c8be572832b773193d7bf579ef0d1a7c7cd
Miguel Mamani <miguel.mamani@mercadofavo.com>
2022-06-01 13:52:20 -0500

    Added orm mapping

--- src/main/java/com/truextend/scheduling/entity/Course.java
+++ src/main/java/com/truextend/scheduling/entity/Course.java
@@ -1,5 +1,31 @@
 package com.truextend.scheduling.entity;
 
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TEXT_COURSE")
 public class Course {
+	
+	@Id
+	@Column(name = "code")
+	private String code;
+	
+	@Column(name = "title")
+	private String title;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@ManyToMany(mappedBy = "courses")
+    private Set<Student> students;
 
 }
--- src/main/java/com/truextend/scheduling/entity/Student.java
+++ src/main/java/com/truextend/scheduling/entity/Student.java
@@ -1,5 +1,41 @@
 package com.truextend.scheduling.entity;
 
-public class Student {
+import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TEXT_STUDENT")
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_id_seq")
+    @SequenceGenerator(name = "student_id_seq", sequenceName = "TEXT_STUDENT_SEQ")
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@ManyToMany
+	@JoinTable(
+	  name = "TEXT_STUDENT_COURSE", 
+	  joinColumns = @JoinColumn(name = "student_id"), 
+	  inverseJoinColumns = @JoinColumn(name = "course_code"))
+    private Set<Course> courses;
 }
